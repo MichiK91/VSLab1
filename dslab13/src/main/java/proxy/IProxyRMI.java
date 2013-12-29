992f18df6b7ae21f8c79a3725398bea785e5a247
MichiK91 <scr4ever@hotmail.com>
2013-12-29 15:53:40 +0100

    2nd try

---
+++ dslab13/src/main/java/proxy/IProxyRMI.java
@@ -0,0 +1,29 @@
+package proxy;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import cli.Command;
+
+import message.Response;
+
+public interface IProxyRMI extends Remote {
+	
+	@Command
+	Response readQuorum() throws RemoteException;
+	
+	@Command
+	Response writeQuorum() throws RemoteException;
+	
+	@Command
+	Response topThreeDownloads() throws RemoteException;
+	
+	@Command
+	Response subscribe(String filename, long numberOfDownloads) throws RemoteException;
+
+	@Command
+	Response getProxyPublicKey() throws RemoteException;
+	
+	@Command
+	Response setUserPublicKey(String username) throws RemoteException;
+}
