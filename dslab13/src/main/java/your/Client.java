5fd32051260d5adb81a43e644a059ac55bb998a3
MichiK91 <scr4ever@hotmail.com>
2013-12-30 13:48:38 +0100

    removed bug

--- dslab13/src/main/java/your/Client.java
+++ dslab13/src/main/java/your/Client.java
@@ -14,17 +14,17 @@ import client.ClientRMI;
 public class Client {
 
 	public static void main(String[] args) {
-		ClientRMI rmi = new ClientRMI();
-//		Config config = new Config("client");
-//		Shell shell = new Shell("client", new DataOutputStream(System.out), new DataInputStream(System.in));
-//
-//		ComponentFactory fact = new ComponentFactory();
-//
-//		try {
-//			fact.startClient(config, shell);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
+//		ClientRMI rmi = new ClientRMI();
+		Config config = new Config("client");
+		Shell shell = new Shell("client", new DataOutputStream(System.out), new DataInputStream(System.in));
+
+		ComponentFactory fact = new ComponentFactory();
+
+		try {
+			fact.startClient(config, shell);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
