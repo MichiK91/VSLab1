cd6cbfe3e549b1faefcd33b86b66590a8ab18302
Matthes <matthes_koenen@yahoo.de>
2014-01-07 14:49:50 +0100

    start stage5

--- dslab13/src/main/java/server/FileServerCli.java
+++ dslab13/src/main/java/server/FileServerCli.java
@@ -4,37 +4,36 @@ import java.io.IOException;
 import java.net.SocketException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import util.Config;
 
 import message.response.MessageResponse;
+import util.Config;
 import cli.Command;
 import cli.Shell;
 
 public class FileServerCli implements IFileServerCli {
 	private Shell shell;
-		
-	//thread pool
+
+	// thread pool
 	private ExecutorService threads = Executors.newCachedThreadPool();
 
 	private FileServerImpl fileserver;
 
 	private TCPHandler handler;
-	
-	public FileServerCli(Config config,Shell shell) throws SocketException{
+
+	public FileServerCli(Config config, Shell shell) throws SocketException {
 		this.shell = shell;
-		
-		//register the shell
+
+		// register the shell
 		this.shell.register(this);
 		this.threads.execute(this.shell);
-		
+
 		fileserver = new FileServerImpl(config);
-		
+
 		this.handler = new TCPHandler(config);
 		this.threads.execute(this.handler);
-		
+
 	}
-	
-	
+
 	@Override
 	@Command
 	public MessageResponse exit() throws IOException {
