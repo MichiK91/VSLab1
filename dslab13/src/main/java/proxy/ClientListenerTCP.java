6acb6c2ace146e8fe322774920f3abd123b7cfaf
MichiK <m.kirchknopf91@gmx.at>
2014-01-08 20:04:38 +0100

    fix

--- dslab13/src/main/java/proxy/ClientListenerTCP.java
+++ dslab13/src/main/java/proxy/ClientListenerTCP.java
@@ -19,57 +19,58 @@ public class ClientListenerTCP implements Closeable, Listener {
 
 	}
 
-	
+
 	@Override
 	public void close() throws IOException {
-		
-		strout.close();
-		strin.close();
-		csocket.close();
+		if(csocket != null){
+			strout.close();
+			strin.close();
+			csocket.close();
+		}
 	}
 
-  @Override
-  public void listen(Object req) throws IOException {
-    try {
-      strout.writeObject(req);
-    } catch (Exception e) {
-      //proxy already closed
-      System.out.println("Proxy has gone offline");
-    }
-  }
+	@Override
+	public void listen(Object req) throws IOException {
+		try {
+			strout.writeObject(req);
+		} catch (Exception e) {
+			//proxy already closed
+			System.out.println("Proxy has gone offline");
+		}
+	}
 
-  @Override
-  public Object receive() {
-    try {
-      Object o = strin.readObject();
+	@Override
+	public Object receive() {
+		try {
+			Object o = strin.readObject();
 
-      if(o instanceof byte[] || o instanceof MessageWrapper){
-        return o;
-      }
-    } catch (ClassNotFoundException e)  {
-      e.printStackTrace();
-    }catch (IOException e)  {
-      try {
-        close();
-      } catch (IOException e1) {
-        e1.printStackTrace();
-      }
-    }
-    return null;//unreachable
-  }
-  
-  public ObjectInputStream getInputStream(){
-    return this.strin;
-  }
-  
-  public void setSocket(Socket socket){
-    this.csocket = socket;
-    try {
-      strin = new ObjectInputStream(csocket.getInputStream());
-      strout = new ObjectOutputStream(csocket.getOutputStream());
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
+			if(o instanceof byte[] || o instanceof MessageWrapper){
+				return o;
+			}
+		} catch (ClassNotFoundException e)  {
+			e.printStackTrace();
+		}catch (IOException e)  {
+			try {
+				close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+		return null;//unreachable
+	}
+
+	public ObjectInputStream getInputStream(){
+		return this.strin;
+	}
+
+	public void setSocket(Socket socket){
+		this.csocket = socket;
+		try {
+			strin = new ObjectInputStream(csocket.getInputStream());
+			strout = new ObjectOutputStream(csocket.getOutputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
