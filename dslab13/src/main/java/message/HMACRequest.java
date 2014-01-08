f0b095aee7a501cf1775e8b7058100a32e126d7f
MichiK <m.kirchknopf91@gmx.at>
2014-01-08 14:30:22 +0100

    stage3 already finished

---
+++ dslab13/src/main/java/message/HMACRequest.java
@@ -0,0 +1,40 @@
+package message;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+
+public class HMACRequest implements Request {
+	
+	private byte[] hmac;
+	private Request req;
+	
+	public HMACRequest(byte[] hmac, Request req){
+		this.hmac = hmac;
+		this.req = req;
+	}
+
+	public byte[] getHmac() {
+		return hmac;
+	}
+
+	public void setHmac(byte[] hmac) {
+		this.hmac = hmac;
+	}
+
+	public Request getReq() {
+		return req;
+	}
+
+	public void setReq(Request req) {
+		this.req = req;
+	}
+
+	@Override
+	public String toString() {
+		return "HMACRequest [hmac=" + Arrays.toString(hmac) + ", req=" + req
+				+ "]";
+	}
+	
+
+}
